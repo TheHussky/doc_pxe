a39e321c8a3f829b887a2fc911b2ab310da03e70
Андрей Кречетов <apkrechetov@miem.hse.ru>
2021-12-01 21:41:23 +0300

    corrections

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,19 @@ FROM ubuntu:20.04
 # Get neeedd packages
 RUN apt-get -y update \
     && apt-get -y install dnsmasq python3 wget pxelinux iproute2
-# Extract vmlinuz and initrd from downloaded iso 
-# https://askubuntu.com/questions/1238070
-RUN mkdir /tftp \
-    && wget http://old-releases.ubuntu.com/releases/20.04/ubuntu-20.04-live-server-amd64.iso -O /tftp/ubuntu-20.04-live-server-amd64.iso \
-    && mount /tftp/ubuntu-20.04-live-server-amd64.iso /mnt \ 
-    && cp /mnt/casper/vmlinuz /tftp \
-    && cp /mnt/casper/initrd /tftp \ 
-    && umount /mnt \
-    && cp /usr/lib/PXELINUX/gpxelinux.0 /tftp/pxelinux.0.bios \ 
+
+RUN wget -O tftp.tar.gz -nv http://archive.ubuntu.com/ubuntu/dists/focal-updates/main/installer-amd64/20101020ubuntu614.3/legacy-images/netboot/netboot.tar.gz
+RUN mkdir /tftp && wget http://old-releases.ubuntu.com/releases/20.04/ubuntu-20.04-live-server-amd64.iso -O /tftp/ubuntu-20.04-live-server-amd64.iso 
+#set right locations
+RUN mkdir temp \
+
+    && tar -xzvf tftp.tar.gz -C temp/ \
+    && mv temp/ubuntu-installer/amd64/* /tftp \
+    && mv /tftp/linux /tftp/vmlinuz \
+    && rm -r temp && rm -rd /tftp/boot-screens \
+    && rm /tftp/pxelinux.cfg/default \
+    && gunzip -d /tftp/initrd.gz \
+    && cp /usr/lib/PXELINUX/pxelinux.0 /tftp/pxelinux.0 \ 
     && cp /usr/lib/syslinux/modules/bios/*.c32 /tftp
 
 COPY tftp/ tftp/
--- pyconf.py
+++ pyconf.py
@@ -1,35 +1,34 @@
 import os
+DNS={
+    'compute-2':'00:1e:67:10:cc:9c'
+}
 
-def ip_to_hex(ip_adress:str, hostname='hostname')->str:
+def ip_to_hex(ip_adress: str, hostname='hostname') -> str:
     """
     Creates a string from ip address according to PxeLinux Docs.
     Returns it or hostname if failed
     """
     try:
-        subnets=ip_adress.split('.')
+        subnets = ip_adress.split('.')
     except:
         print("Given ip adress is wrong! Server will be using default file.")
         return hostname
-    hex_code=''
+    hex_code = ''
     for subnet in subnets:
-        subnet_code=str(hex(int(subnet))).upper()[2:]
-        if len(subnet_code)==1:
-            subnet_code='0'+subnet_code
-        hex_code+=subnet_code
+        subnet_code = str(hex(int(subnet))).upper()[2:]
+        if len(subnet_code) == 1:
+            subnet_code = '0'+subnet_code
+        hex_code += subnet_code
     return hex_code
-
-
 
 
 # Starting params
 default = open('/tftp/pxelinux.cfg/default', 'r')
-default_confs=default.readlines()
+default_confs = default.readlines()
 default.close()
 
 # Obtain list of hosts for this conf
 hosts = os.listdir('/tftp/cloud-init-bios/user-data')
-
-
 
 
 ip = 10
@@ -37,16 +36,19 @@ for host in hosts:
     ip += 1
 
     # Create address and write it to dnsmasq
-    addr=f"dhcp-host={host},10.70.30.{ip}"
-    dns=open('/etc/dnsmasq.conf', 'a')
+    try:
+        addr = f"dhcp-host={DNS[host]},10.70.30.{ip}"
+    except:
+        addr = f"dhcp-host={host},10.70.30.{ip}"
+    dns = open('/etc/dnsmasq.conf', 'a')
     dns.write(addr+'\n')
+    addr=addr.split(',')[1]
     dns.close()
 
     # Create pxelinux conf files based on default and link user-datas
-    host_conf=open(f"/tftp/pxelinux.cfg/{ip_to_hex(addr)}", 'w')
-    default_confs[-1]=default_confs[-1].replace('$user-data', host)
+    host_conf = open(f"/tftp/pxelinux.cfg/{ip_to_hex(addr)}", 'w')
+    default_confs[-1] = default_confs[-1].replace('$user-data', host)
     for line in default_confs:
         host_conf.write(line)
-    default_confs[-1]=default_confs[-1].replace('$user-data', host)
+    default_confs[-1] = default_confs[-1].replace('$user-data', host)
     host_conf.close()
-    
