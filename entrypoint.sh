02f9fd4909351c441f102f967fa953a4ca56462c
mmskv <max.mishuk2014@gmail.com>
2021-12-01 19:55:08 +0300

    Add networking exception

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$(ip a | grep 10.70.30)" ]; then
+    >&2 echo "Error: no interface is configured to use 10.70.30.1/24"
+    exit 1
+fi
+
 sed -i "s/\$INTERFACE/$(ip a | grep 10.70.30 | awk '{ print $5 }')/g" /etc/dnsmasq.conf
 
 python3 pyconf.py
